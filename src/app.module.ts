061c58c8205142cae98a365bcbb856cde8bfb970
Aleksey Tsvetkov <code.aleksey@icloud.com>
2022-09-19 22:21:04 +0300

    feat(global): graphql and user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
 import { config } from './config';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -10,10 +15,20 @@ import { PrismaModule } from './prisma/prisma.module';
       isGlobal: true,
       load: config,
     }),
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      imports: [AuthModule],
+      inject: [AuthService],
+      useFactory: async () => ({
+        installSubscriptionHandlers: true,
+        // validationRules: [depthLimit(10)],
+        autoSchemaFile: join(process.cwd(), './schema.gql'),
+        context: (ctx) => ctx?.extra?.socket?.ctx,
+      }),
+    }),
     AuthModule,
     PrismaModule,
+    UserModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
