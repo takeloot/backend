d635ab5768d88241629e49b30187e945c52fc788
Aleksey Tsvetkov <code.aleksey@icloud.com>
2022-10-23 19:35:16 +0300

    feat(global): added user queries

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UserResolver } from './user.resolver';
+import { UserService } from './user.service';
 
 @Module({
   imports: [PrismaModule, AuthModule],
   controllers: [],
-  providers: [UserResolver],
+  providers: [UserResolver, UserService],
   exports: [],
 })
 export class UserModule {}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -4,4 +4,45 @@ import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
+
+  async getUsers() {
+    return await this.prisma.user.findMany();
+  }
+
+  async getUserById(id: string, userId: string) {
+    if (!id) {
+      if (!userId) return null;
+      id = userId;
+    }
+
+    return await this.prisma.user.findFirst({
+      where: { id },
+      include: { profiles: true },
+    });
+  }
+
+  async getMe(userId: string) {
+    return await this.prisma.user.findFirst({
+      where: { id: userId },
+      include: { profiles: true },
+    });
+  }
+
+  async updateMyTradeUrl(tradeUrl: string, userId: string) {
+    const steamTradeUrlRegex =
+      /https?:\/\/steamcommunity.com\/tradeoffer\/new\/\?partner=(\d+)&token=(.{8})$/;
+
+    if (!tradeUrl || !tradeUrl.match(steamTradeUrlRegex)) {
+      throw new Error('Invalid trade Url');
+    }
+
+    await this.prisma.user.update({
+      where: { id: userId },
+      data: {
+        tradeUrl,
+      },
+    });
+
+    return true;
+  }
 }
